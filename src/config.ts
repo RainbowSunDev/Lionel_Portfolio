5dbfdec1aa611ab4d43d06dda64ad7307487bb45
DESKTOP-4JU8VDC\khg <rainbowsun.pop@gmail.com>
2023-03-14 09:46:22 -0700

    First_Commit

---
+++ src/config.ts
@@ -0,0 +1,32 @@
+const siteOwnerName = "Patrick Obermeier";
+
+const config = {
+  flags: {
+    useMockData: true,
+  },
+  gtm: {
+    id: "GTM-TLPT94R",
+  },
+  meta: {
+    bingValidationId: "D5238646980530643F5FC0FFBF63FB97",
+    canonicalRootUrl: "https://www.patrickobermeier.at",
+    defaultTitle: `Senior Frontend Developer | ${siteOwnerName}`,
+    defaultDescription: `${siteOwnerName} is an experienced front-end developer and online-marketing expert from Vienna, Austria. Creating web apps and websites using latest technologies like React & the MERN-Stack with passion!`,
+    defaultKeywords:
+      "frontend developer, front-end developer, web developer, webdesign, web designer, html5, css, javascript, react, redux, mern-stack, node, mongodb, express, docker, linux, portfolio, projects, coder, programmer, web apps, websites, marketing sites, landing pages",
+    siteOwnerName,
+    twitterHandle: "@Pobermeier87",
+    socialShareImgUrl: "https://www.datocms-assets.com/60761/1642858513-siteshot.png",
+    themeColor: "#111827",
+  },
+  urls: {
+    email: "mailto:office@patrickobermeier.at",
+    fakeEmail: "mailto:contact@patrickobermeier.dev",
+    linkedIn: "https://www.linkedin.com/in/patrickobermeier/",
+    github: "https://github.com/Pobermeier",
+    twitter: "https://twitter.com/pobermeier87",
+    websiteRepo: "https://github.com/Pobermeier/pobermeier-portfolio-v3",
+  },
+};
+
+export default config;
